81a0a7b1bbaa871c1365f8da5ee3974cad13867e
ghostayush <ayushsb4451@gmail.com>
2024-08-10 16:50:41 +0530

    Initial commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,61 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Dev Detection",
+        description:"Detection website for finding someone's github profile and accesing public information of that profile ,all in one place.",
+        tags:["react","styledComponents","API"],
+        
+        demo:"https://ghostayush.github.io/DEV-Detection/",
+        github:"https://github.com/ghostayush/DEV-Detection"
+    },
+    {
+        id:2,
+        name:"GIF Generator",
+        description:"This website is a convenient tool for generating random GIF instantly,also allowing users to search specific GIF easily.",
+        tags:["react","API"],
+        
+        demo:"https://ghostayush.github.io/GIF-Generator/",
+        github:"https://github.com/ghostayush/GIF-Generator"
+        
+    },
+    {
+        id:3,
+        name:"Random password Generator",
+        description:"This website is a convenient tool for generating random password instantly, allowing users to copy the generated password easily.",
+        tags:["HTML","Javascript","css"],
+        
+        demo:"https://ghostayush.github.io/random-password-generator/",
+        github:"https://github.com/ghostayush/random-password-generator"
+    },
+    {
+        id:4,
+        name:"Tic Tac Toe",
+        description:"This project is the classic game built for entertainment of two people.The tic-tac-toe App is a user-friendly application.",
+        tags:["HTML","Javascript","css"],
+        
+        demo:"https://ghostayush.github.io/tic-tac-toe/",
+        github:"https://github.com/ghostayush/tic-tac-toe"
+       
+    },{
+        id:5,
+        name:"E-Commerce",
+        description:"An eCommerce website built with the MERN stack featuring a robust admin portal for seamless product management and order tracking.",
+        tags:["MERN","redux","Tailwind"],
+        
+        demo:"https://github.com/ghostayush/MERN-E-Shop",
+        github:"https://github.com/ghostayush/MERN-E-Shop"
+        
+    },
+    {
+        id:6,
+        name:"AI-Carrier-Guide",
+        description:"A personalized career guidance app designed to help 10th and 12th-grade students make informed decisions about their future paths.",
+        tags:["react","python","css"],
+        
+        demo:"https://github.com/ghostayush/Career-Sage-home",
+        github:"https://github.com/ghostayush/Career-Sage-home"
+        
+    },
+] 
